d58234162626d9c4643b132051f6071618b3b793
Naol Dame <naododa5566@gmail.com>
2022-06-04 22:29:24 +0300

    Folder structure FindLoad

--- techlink/src/Screens/signup_login/findLoad/findLoad.js
+++ techlink/src/Screens/signup_login/findLoad/findLoad.js
@@ -1,26 +1,11 @@
 import * as React from "react";
 import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 // import Icon from "react-native-vector-icons/FontAwesome";
-
+import styles from "./style";
 export default function App() {
   return (
     <View style={{ paddingTop: 60, paddingBottom: 40, paddingHorizontal: 15 }}>
-      <View
-        style={{
-          backgroundColor: "white",
-          width: "100%",
-          height: "100%",
-          borderColor:'#1e128a',borderRadius:60,
-          borderLeftWidth:0.7,
-          borderTopWidth:0.5,
-          borderTopRightRadius:60,
-          borderBottomRightRadius:60,
-          // borderRadius:60,
-          borderRightWidth:6,
-          borderBottomWidth:6
-         
-        }}
-      >
+      <View style={styles.parentview}>
         <View
           style={{
             flex: 3,
@@ -28,93 +13,28 @@ export default function App() {
             alignItems: "center",
           }}
         >
-          <Image source={require('./assets/techpkg.png')} style={{width:200,height:150}}></Image>
-         
+          <Image
+            source={require("./assets/techpkg.png")}
+            style={{ width: 200, height: 150 }}
+          ></Image>
+
           <Text style={{ fontSize: 25, marginTop: 50, fontWeight: "bold" }}>
             Find load to deliver{" "}
           </Text>
-         
         </View>
-        <View style={{ flex: 1}}>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-evenly",
-              marginTop: 10,
-              padding: 20,
-            }}
-          >
-            <TouchableOpacity
-              style={{
-                backgroundColor: "white",
-                width: '40%',
-                height: 50,
-                alignItems: "center",
-                justifyContent: "center",
-                borderColor: "#1e128a",
-                borderRadius: 15,
-
-                borderRightWidth: 3,
-                borderBottomWidth: 4,
-                borderTopWidth: 0.5,
-                borderLeftWidth: 0.5,
-              }}
-            >
-              <Text style={{fontWeight:"bold"}}>Skip</Text>
+        <View style={{ flex: 1 }}>
+          <View style={styles.FindLoadView}>
+            <TouchableOpacity style={styles.Skipbtn}>
+              <Text style={{ fontWeight: "bold" }}>Skip</Text>
             </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                width: "40%",
-
-                height: 50,
-                alignItems: "center",
-                justifyContent: "center",
-                backgroundColor: "#1e128a",
-                borderColor: "#1697f2",
-                borderRadius: 15,
-
-                borderRightWidth: 3,
-                borderBottomWidth: 3,
-                borderTopWidth: 0.5,
-                borderLeftWidth: 0.5,
-              }}
-            >
-              <Text style={{ color: "white",fontWeight:'bold' }}>Next</Text>
+            <TouchableOpacity style={styles.Nextbtn}>
+              <Text style={{ color: "white", fontWeight: "bold" }}>Next</Text>
             </TouchableOpacity>
           </View>
-          <View
-            style={{
-              alignItems: "center",
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
-            <Text
-              style={{
-                backgroundColor: "#db6e3b",
-                width: 9,
-                height: 9,
-                borderRadius: 100,
-                marginRight: 5,
-              }}
-            ></Text>
-            <Text
-              style={{
-                backgroundColor: "#adaaa8",
-                width: 8,
-                height: 8,
-                borderRadius: 100,
-                marginRight: 5,
-              }}
-            ></Text>
-            <Text
-              style={{
-                backgroundColor: "#adaaa8",
-                width: 8,
-                height: 8,
-                borderRadius: 100,
-              }}
-            ></Text>
+          <View style={styles.SlideContainer}>
+            <Text style={styles.Slide1}/>
+            <Text style={styles.Slide2}/>
+            <Text style={styles.slide3}/>
           </View>
         </View>
       </View>
