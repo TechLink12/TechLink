eac334a4bc8431407658d6bbb94e5766bc65dce7
Naol Dame <naododa5566@gmail.com>
2022-06-06 23:20:23 +0300

    installing and creating react maps

---
+++ techlink/src/components/general/constants/colors.js
@@ -0,0 +1,12 @@
+export default {
+    primary: '#0c0e70',
+    secondary: '#F2F7F9',
+    orange: '#f7792a',
+    black: '#000000',
+    dark: '#222222',
+    darkerBlue: '#005E9F',
+    gray: '#8d8d8f',
+    lightGray: '#9EADBD',
+    white: '#FFFFFF',
+    whiteSmoke: '#F5F5F5',
+  };
